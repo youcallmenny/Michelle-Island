dad97e4cbb410dcc5d4517bf71fda24e862bb667
youcallmenny <96740437+youcallmenny@users.noreply.github.com>
2025-08-26 22:02:56 -0700

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='m-colony-v5';
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=1.2.2','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'])));
-});
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-});
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
