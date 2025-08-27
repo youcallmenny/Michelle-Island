bb1eb551b3fc8b622a650eedb05924c42f240fa5
youcallmenny <96740437+youcallmenny@users.noreply.github.com>
2025-08-26 22:09:49 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='m-colony-v6'; // bump cache to force update
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
+    './','./index.html?v=1.2.3','./manifest.json',
+    './icons/icon-192.png','./icons/icon-512.png','./icons/chibi_michelle_64.png'
+  ])));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
